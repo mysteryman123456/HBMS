d88eb852fe3e92d046f104b509b68e3cf4f6e379
SiteDecors <nschldhl61@gmail.com>
2024-12-20 22:37:01 +0545

    Signup and login frontend code added

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import { useState , useEffect } from 'react'
 const Login = () => {
+  const[eye , setEye] = useState(false);
+  const[loginData , setLoginData] = useState({
+    email : "",
+    password : "",
+  });
+  const handleChange = (event) =>{
+    const {name , value} = event.currentTarget;
+    setLoginData((prevData)=>({...prevData , [name] : value})
+    )
+  }
+  const handleLogin=()=>{
+    console.log(loginData)
+  }
   return (
-    <div>
-      
+<div className="signup-container">
+        <h1 className="signup-title">Login</h1>
+        <input onChange={handleChange} name='email' type="email" placeholder="Email" className="signup-input" />
+        <div className='password-wrapper'>
+          <input onChange={handleChange} name='password' style={{border:"0",margin:"0px"}} type={!eye ? "password" : "text"} placeholder="Password" className="signup-input" />
+          <div onClick={()=>(setEye(!eye))}>{eye ? <><i className='ri-eye-line'></i></> : <><i className='ri-eye-close-line'></i></>}</div>
+        </div>
+        <button onClick={handleLogin} className="signup-button">Login</button>
+        <p className="signup-login">
+          Don't have an account? <Link to="/signup">Signup</Link>
+        </p>
     </div>
   )
 }
-
 export default Login
--- frontend/src/Components/Signup.jsx
+++ frontend/src/Components/Signup.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import React from 'react';
+import { useState , useEffect } from 'react';
+import {Link} from 'react-router-dom';
 const Signup = () => {
+  const[eye , setEye] = useState(false);
+  const[signupData , setSignupData] = useState({
+    email : "",
+    password : "",
+    confirm_password : "",
+    fullname : "",
+    role : "",
+    phonenumber : "",
+  });
+  const handleChange = (event) =>{
+    const {name , value} = event.currentTarget;
+    setSignupData((prevData)=>({...prevData , [name] : value})
+    )
+  }
+  const handleSignup=()=>{
+    console.log(signupData)
+  }
   return (
-    <div>
-      
+    <div className="signup-container">
+        <h1 className="signup-title">Signup</h1>
+        <input onChange={handleChange} name='fullname' type="text" placeholder="Full Name" className="signup-input" />
+        <input onChange={handleChange}  name='email' type="email" placeholder="Email" className="signup-input" />
+        <input onChange={handleChange}  name='phonenumber' type="text" placeholder="Phonenumber" className="signup-input" />
+        <div className='password-wrapper'>
+          <input onChange={handleChange}  name='password' style={{border:"0",margin:"0px"}} type={!eye ? "password" : "text"} placeholder="Password" className="signup-input" />
+          <div onClick={()=>(setEye(!eye))}>{eye ? <><i className='ri-eye-line'></i></> : <><i className='ri-eye-close-line'></i></>}</div>
+        </div>
+        <input onChange={handleChange}  type="password" name='confirm_password' placeholder="Confirm password" className="signup-input" />
+        <select name='role' onChange={handleChange}>
+          <option value="" disabled>
+            Choose role
+          </option>
+          <option value="user">User</option>
+          <option value="hotel_admin">Hotel Admin</option>
+        </select>
+        <button onClick={handleSignup} className="signup-button">Signup</button>
+        <p className="signup-login">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;

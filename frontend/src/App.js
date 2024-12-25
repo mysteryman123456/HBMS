5a799fd8f550865bf51bed3bed67c04ca1f672af
Bishnumahato98 <bishnukumarmahato262@gmail.com>
2024-12-25 17:00:25 +0545

    Admin page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Components/Footer"
 import Signup from "./Components/Signup"
 import Login from "./Components/Login"
 import HotelAdminDashboard from "./Components/HotelAdminDashboard"
+import HotelListing from "./Components/HotelLisitng"
 import UserDashboard from "./Components/UserDashboard"
 import ProductPage from "./Components/ProductPage"
 import SearchPage from "./Components/SearchPage"
@@ -20,7 +21,11 @@ function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/user-dashboard" element={<UserDashboard />} />
-            <Route path="/hotel-admin-dashboard" element={<HotelAdminDashboard />} />
+            <Route path="/hotel-admin-dashboard/" element={<HotelAdminDashboard />}>
+                <Route path="add-listing" element={<HotelListing />} />
+                <Route index element={<HotelListing />} />
+                <Route path="*" element={<NotFound />} />
+            </Route> 
             <Route path="/search" element={<SearchPage />} />
             <Route path="/hotel/:id" element={<ProductPage />} />
           </Routes>
